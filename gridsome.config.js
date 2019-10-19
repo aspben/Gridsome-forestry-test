08651606ef5bda01ec4fa9c0d29637484e61433f
aspben <31743275+aspben@users.noreply.github.com>
2019-10-18 22:48:19 -0400

    git push try

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
+  siteUrl: `https://www.blog.sportmngr.com`,
   host: "0.0.0.0",
   titleTemplate: "%s - NWA",
   siteDescription: "Creative technologist",
